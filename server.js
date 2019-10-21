1370422b70d0deb58c2f2ecd08365c8a4940a792
prahllad roy <royprahllad3@gmail.com>
2019-10-21 23:37:53 +0530

    changes

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = require('./app');
 const https = require('https');
 const fs = require('fs');
 const port = process.env.PORT || 3002;
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app).listen(port, () => {
+https.createServer( app).listen(port, () => {
     console.log('Listening...')
   });
   console.log('Express backend server started in ' + app.get('env') + ' mode on port ' + port);
 
   //used this command to generate server.key and server.cert for local certficate installation
   //openssl req -nodes -new -x509 -keyout server.key -out server.cert
+  // //{
+  //   key: fs.readFileSync('server.key'),
+  //   cert: fs.readFileSync('server.cert')
+  // },
